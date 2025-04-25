a2b7e0bab65090daa238037af7ea66560ebe56c1
Ammar Mahmoud <ammarma.eid@gmail.com>
2025-04-25 14:05:21 +0300

    home

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/CV/', // Set this to your repository name
+    base: "/CV", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
